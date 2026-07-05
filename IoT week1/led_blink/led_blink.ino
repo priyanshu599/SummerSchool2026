d5e22b4193b9118cf8b590331f2399cc3116042d
Priyanshu <py71497@gmail.com>
2026-07-06 01:09:38 +0530

    feat: control blink speed using potentiometer

--- IoT week1/led_blink/led_blink.ino
+++ IoT week1/led_blink/led_blink.ino
@@ -1,20 +1,31 @@
 int blinkCount = 0;
 
 void setup() {
+
   pinMode(13, OUTPUT);
+
   Serial.begin(9600);
+
 }
 
 void loop() {
 
-  digitalWrite(13, HIGH);
-  delay(500);
+  int sensorValue = analogRead(A0);
 
-  digitalWrite(13, LOW);
-  delay(500);
+  int delayTime = map(sensorValue,0,1023,100,1000);
+
+  digitalWrite(13,HIGH);
+  delay(delayTime);
+
+  digitalWrite(13,LOW);
+  delay(delayTime);
 
   blinkCount++;
 
   Serial.print("Blink count: ");
-  Serial.println(blinkCount);
+  Serial.print(blinkCount);
+
+  Serial.print(" Delay: ");
+  Serial.println(delayTime);
+
 }
